daa1994e40a2c2de47c01d4442e81d546391b9ec
Carlos Portella <carlosportella16@hotmail.com>
2022-04-15 15:58:08 -0300

    initial commit crud cidades

---
+++ src/main/java/visao/CidadeController.java
@@ -0,0 +1,14 @@
+package visao;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CidadeController {
+
+    @GetMapping("/")
+    public String listar() {
+        return "crud.html";
+    }
+    
+}
---
+++ src/test/java/com/espjava/crudcidades/CrudCidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espjava.crudcidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
