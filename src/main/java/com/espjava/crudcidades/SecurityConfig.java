e8f9165a2984aec8a3eed3b4655e454ba7002f87
Carlos <carlosportella16@hotmail.com>
2022-05-24 00:10:16 -0300

    Authentication with Database

--- src/main/java/com/espjava/crudcidades/SecurityConfig.java
+++ src/main/java/com/espjava/crudcidades/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.espjava.crudcidades;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,34 +13,31 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication()
-        .withUser("john")
-        .password(cifrador().encode("test123"))
-        .authorities("listar")
-        .and()
-        .withUser("anna")
-        .password(cifrador().encode("test123"))
-        .authorities("admin");
-  }
-
-  @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
         .csrf().disable()
         .authorizeRequests()
         .antMatchers("/").hasAnyAuthority("listar", "admin")
         .antMatchers("/criar").hasAuthority("admin")
-        .antMatchers("/excluir").hasAnyAuthority("admin")
+        .antMatchers("/excluir").hasAuthority("admin")
         .antMatchers("/preparaAlterar").hasAuthority("admin")
         .antMatchers("/alterar").hasAuthority("admin")
         .anyRequest().denyAll()
         .and()
-        .formLogin().permitAll();
+        .formLogin()
+        .loginPage("/login.html").permitAll()
+        .defaultSuccessUrl("/", false)
+        .and()
+        .logout().permitAll();
   }
 
   @Bean
   public PasswordEncoder cifrador() {
     return new BCryptPasswordEncoder();
   }
+
+  // @EventListener(ApplicationReadyEvent.class)
+  // public void printSenhas() {
+  // System.out.println(this.cifrador().encode("test123"));
+  // }
 }
---
+++ src/main/java/com/espjava/crudcidades/usuario/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.espjava.crudcidades.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+  
+  public Usuario findByNome(String nome);
+}
