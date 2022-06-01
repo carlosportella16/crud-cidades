abee8a47ebad495892fc11cb7547659f1b40f6b3
Carlos <carlosportella16@hotmail.com>
2022-05-31 23:04:38 -0300

    creating metod to show cookies

--- src/main/java/com/espjava/crudcidades/cidade/CidadeController.java
+++ src/main/java/com/espjava/crudcidades/cidade/CidadeController.java
@@ -14,9 +14,11 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CidadeController {
@@ -134,6 +136,12 @@ public class CidadeController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping("/mostrar")
+    @ResponseBody
+    public String mostrarCookie(@CookieValue String listar) {
+        return "Último acesso ao método listar(): " + listar;
     }
 
 }
